5954a6af3e4aa2080dc644b87f493fdbda18f366
HamzaR19 <m.hamza.rahman@gmail.com>
2023-05-18 23:05:00 -0500

    Fixing the code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # PasswordGenerator
+
+THIS IS A PASSWORD GENERATOR
+
+This is program that generates a password
+You enter the appropiate values in the prompt
+You are then asked if you would like to use Uppercase
+You are then asked if you would like to use Numbers
+After you have selected each option or chose to cancel which results in a "no"
+You are presented with your new generated password
+
+
+Have a great day!
--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
-var upperCaseSet = ["A","B","C"];
-var numberSet = [1,2,3]
-var userPickedCharacterSet = []
-// sets password length
-var pLength = parseInt(window.prompt("enter a number between 8 and 128") )
-console.log(typeof pLength);
- // run frogram to check for number, if false redirect for a number
-    if (pLength >= 1 && pLength <= 150) {
-       console.log(pLength);
-      //  Confirming uppercase
-      var isUpperCase = confirm("Do you want uppercase?")
-      console.log(isUpperCase);
-      var isNumber = confirm("Do you want numbers?")
-      console.log(isNumber);
-      // Two different outlooks
-      if (isUpperCase || isNumber) {
-        console.log("working");
-        if (isUpperCase) {
-          userPickedCharacterSet = userPickedCharacterSet.concat(upperCaseSet)
-        }
-        if (isNumber) {
-          userPickedCharacterSet = userPickedCharacterSet.concat(numberSet)
-        }
-        console.log(userPickedCharacterSet);
-        for (var i = 0; i < pLength; i++) {
-          console.log("hello");
-        var randomChar = userPickedCharacterSet[Math.floor(Math.random()*userPickedCharacterSet.length)]
-      console.log(randomChar);
+  var upperCaseSet = ["A", "B", "C"];
+  var numberSet = [1, 2, 3];
+  var userPickedCharacterSet = [];
+
+  var pLength = parseInt(prompt("Enter a number between 1 and 150"));
+
+  if (pLength >= 1 && pLength <= 150) {
+    var isUpperCase = confirm("Do you want uppercase letters?");
+    var isNumber = confirm("Do you want numbers?");
+
+    if (isUpperCase || isNumber) {
+      if (isUpperCase) {
+        userPickedCharacterSet = userPickedCharacterSet.concat(upperCaseSet);
       }
-      } else {
-        console.log("Please pick at least 1 character set");
-        generatePassword();
+      if (isNumber) {
+        userPickedCharacterSet = userPickedCharacterSet.concat(numberSet);
       }
+
+      var password = "";
+      for (var i = 0; i < pLength; i++) {
+        var randomChar =
+          userPickedCharacterSet[Math.floor(Math.random() * userPickedCharacterSet.length)];
+        password += randomChar;
+      }
+
+      return password;
+    } else {
+      // If neither uppercase nor number is selected
+      alert("Please select at least one option: uppercase or numbers");
+      return generatePassword();
     }
-    else {
-      alert("Please enter a number between 8 and 128")
-      generatePassword();
-    }
+  } else {
+    // If the entered length is invalid
+    alert("Please enter a number between 1 and 150");
+    return generatePassword();
   }
-// var upperCase = window.prompt("Do you want upper case letters Yes or No");
-//   console.log(upperCase);
-// Write password to the #password input
+}
+
 function writePassword() {
-// alert("working")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
-// function generatePassword() {
-  // return("hello")
